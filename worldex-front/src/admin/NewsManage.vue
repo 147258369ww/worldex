d0a94db9dc544d2f070610c5b3ddb7b66a06fef7
wjm <2080676062@qq.com>
2026-05-16 20:28:21 +0800

    chore: strip toolbar to minimal set (undo/redo, bold/italic/underline/strikethrough, align, link, image)

--- worldex-front/src/admin/NewsManage.vue
+++ worldex-front/src/admin/NewsManage.vue
@@ -43,23 +43,9 @@ import 'tinymce/tinymce'
 import 'tinymce/models/dom/model'
 import 'tinymce/themes/silver'
 import 'tinymce/icons/default'
-import 'tinymce/plugins/advlist'
 import 'tinymce/plugins/autolink'
-import 'tinymce/plugins/lists'
 import 'tinymce/plugins/link'
 import 'tinymce/plugins/image'
-import 'tinymce/plugins/charmap'
-import 'tinymce/plugins/preview'
-import 'tinymce/plugins/anchor'
-import 'tinymce/plugins/searchreplace'
-import 'tinymce/plugins/visualblocks'
-import 'tinymce/plugins/code'
-import 'tinymce/plugins/fullscreen'
-import 'tinymce/plugins/insertdatetime'
-import 'tinymce/plugins/media'
-import 'tinymce/plugins/table'
-import 'tinymce/plugins/help'
-import 'tinymce/plugins/wordcount'
 import Editor from '@tinymce/tinymce-vue'
 
 const langStore = useLangStore()
@@ -71,8 +57,8 @@ const editorConfig = {
   height: 500,
   skin_url: '/tinymce/skins/ui/oxide',
   content_css: '/tinymce/skins/content/default/content.css',
-  plugins: 'advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help wordcount',
-  toolbar: 'undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image table | code fullscreen | help',
+  plugins: 'autolink link image',
+  toolbar: 'undo redo | bold italic underline strikethrough | alignleft aligncenter alignright | link image',
   images_upload_handler: async (blobInfo) => {
     try {
       const file = new File([blobInfo.blob()], blobInfo.filename(), { type: blobInfo.blob().type })
